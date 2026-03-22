8e1ef089f3ffb4d615b33c5dee4e1a6b710f3ab9
Artyom <privetgeubu1@gmail.com>
2026-03-22 23:51:25 +0300

    Added more models

--- shared/src/commonMain/kotlin/com/y/sandboxy/sandboxy/model/AvailableModels.kt
+++ shared/src/commonMain/kotlin/com/y/sandboxy/sandboxy/model/AvailableModels.kt
@@ -10,6 +10,9 @@ object AvailableModels {
         LlmModel("minimax/minimax-m2.5", "Minimax-m2.5"),
         LlmModel("deepseek/deepseek-v3.2", "Deepseek-v3.2"),
         LlmModel("openai/gpt-5.4", "GPT-5.4"),
+        LlmModel("moonshotai/kimi-k2.5", "Kimi-k2.5"),
+        LlmModel("stepfun/step-3.5-flash", "Step-3.5-flash"),
+        LlmModel("nvidia/nemotron-3-super-120b-a12b", "Nvidia-nemotron-3-super")
     )
 
     val default = all.first()
